fc4d7f5f6e4801563f82c0e036a3978b77dd4593
Tom Lowe <tom.lowe@gmail.com>
2015-10-01 22:23:18 -0400

    pass through params to recent activity api call

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
-$.get('https://fast-brushlands-4500.herokuapp.com/recent-activities', function( data ) {
-  var source = $("#recent-activities").html();
-  var template = Handlebars.compile(source);
-  var html = template({recentActivities: data.result});
-  $('#recent-activities-holder').append(html);
+function getParameterByName(name) {
+	var regexS = "[\\?&]" + name + "=([^&#]*)",
+		regex = new RegExp(regexS),
+		results = regex.exec(window.location.search);
+	if (results == null) {
+		return "";
+	} else {
+		return decodeURIComponent(results[1].replace(/\+/g, " "));
+	}
+}
+var paramString = ""
+if (getParameterByName("lead_id")) {
+	paramString = "?lead_id=" + getParameterByName("lead_id")
+} else if (getParameterByName("page_url")) {
+	paramString = "?page_url=" + getParameterByName("page_url")
+}
+$.get('https://fast-brushlands-4500.herokuapp.com/recent-activities' + paramString, function(data) {
+	var source = $("#recent-activities").html();
+	var template = Handlebars.compile(source);
+	var html = template({
+		recentActivities: data.result
+	});
+	$('#recent-activities-holder').append(html);
 });
